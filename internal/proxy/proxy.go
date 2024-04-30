7d6a309710ba2ddcb3362a35a93d47b2e31308ca
Nik <morgenpurple@gmail.com>
2024-04-30 11:43:42 +0300

    internal/proxy/proxy.go: minor changes

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -1,12 +1,12 @@
 package proxy
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"net"
 	"net/http"
 	"strings"
+	"sync"
 
 	"github.com/rs/zerolog/log"
 )
@@ -19,7 +19,7 @@ func ProxyConnectHandler(w http.ResponseWriter, r *http.Request) {
 
 	logger := log.With().Str("from", r.RemoteAddr).Str("to", r.URL.Host).Logger()
 
-	logger.Debug().Msg("Proxying request")
+	logger.Debug().Msg("Proxy request")
 
 	clientConn, _, err := w.(http.Hijacker).Hijack()
 	if err != nil {
@@ -39,10 +39,11 @@ func ProxyConnectHandler(w http.ResponseWriter, r *http.Request) {
 
 	logger.Debug().Msg("Transfer start")
 
-	ctx, cancel := context.WithCancel(context.Background())
-	go copyIO(cancel, targetConn, clientConn)
-	go copyIO(cancel, clientConn, targetConn)
-	<-ctx.Done()
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go copyIO(wg, targetConn, clientConn)
+	go copyIO(wg, clientConn, targetConn)
+	wg.Wait()
 
 	logger.Debug().Msg("Transfer complete")
 }
@@ -54,13 +55,14 @@ func writeStatusLine(conn net.Conn, statusCode int, r *http.Request) {
 	}
 }
 
-func copyIO(cancel context.CancelFunc, dest, src net.Conn) {
-	if _, err := io.Copy(dest, src); err != nil {
+func copyIO(wg *sync.WaitGroup, dst, src net.Conn) {
+	defer wg.Done()
+	if _, err := io.Copy(dst, src); err != nil {
 		if !strings.Contains(err.Error(), "use of closed network connection") {
-			log.Error().Str("local_addr", dest.LocalAddr().String()).Err(err).Msg("")
+			log.Error().Err(err).Msg("")
 		}
 		return
 	}
-	cancel()
+	dst.Close()
 
 }
